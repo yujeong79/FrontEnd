53cf0870e4426dc0ffc497dc1aeafb46e6a336bf
김유정 <aabbc0908@naver.com>
2024-11-06 17:30:37 +0900

    commit

--- vue_ws_4_1/vue-project/src/App.vue
+++ vue_ws_4_1/vue-project/src/App.vue
@@ -1,13 +1,34 @@
 <template>
   <div>
-
+    <h2>App.vue 파일이야~</h2>
+    <button v-on:click="show(0)" v-bind:class="{dark:!flag[0], light:flag[0]}">0번 컴포넌트</button>
+    <button v-on:click="show(1)" v-bind:class="{dark:!flag[1], light:flag[1]}">1번 컴포넌트</button>
+    <button v-on:click="show(2)" v-bind:class="{dark:!flag[2], light:flag[2]}">2번 컴포넌트</button>
+    <MainPage v-show="flag[0]"/>
+    <Main2Page v-show="flag[1]"/>
+    <Main3page v-show="flag[2]"/>
   </div>
 </template>
 
 <script setup>
+  import {ref} from 'vue'
 
+  import MainPage from "@/components/MainPage.vue";
+  import Main2Page from "./components/Main2Page.vue";
+  import Main3page from "./components/Main3Page.vue";
+
+  const flag = ref([false, true, true]);
+  const show = function(idx) {
+    flag.value[idx] = !flag.value[idx];
+  }
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+  .dark {
+    background-color:black;
+    color: white;
+  }
+  .light {
+    background-color: white;
+  }
 </style>
---
+++ vue_ws_4_1/vue-project/src/components/Main3Page.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h2>메인2 페이지</h2>
+        <img src="@/assets/microphone.jpg" width="200">
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
