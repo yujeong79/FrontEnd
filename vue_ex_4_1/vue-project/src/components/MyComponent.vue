9ac8e9655816b868a2d48f88c64c3ee2f76b357a
김유정 <aabbc0908@naver.com>
2024-11-06 11:43:11 +0900

    commit

---
+++ vue_ex_4_1/vue-project/src/components/MyComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <header>
+            <h1>Gallery</h1>
+        </header>
+        <main>
+            <p>카드</p>
+        </main>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
