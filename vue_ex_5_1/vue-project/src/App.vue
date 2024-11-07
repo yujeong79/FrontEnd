4a4b08a540f9e3cd15840a321c2ef2c61788a3b3
김유정 <aabbc0908@naver.com>
2024-11-07 12:27:16 +0900

    commit

---
+++ vue_ex_5_1/vue-project/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>쇼핑 애플리케이션</h1>
+    <ProductList v-bind:product-list="products"/>
+  </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+
+  let id = 0
+  const products = ref([
+    { id: id++, name: '사과', price: 1000 },
+    { id: id++, name: '바나나', price: 1500 },
+    { id: id++, name: '딸기', price: 2000 },
+    { id: id++, name: '포도', price: 3000 },
+    { id: id++, name: '복숭아', price: 2000 },
+    { id: id++, name: '수박', price: 5000 }
+  ])
+
+  import ProductList from '@/components/ProductList.vue';
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue_ex_5_1/vue-project/src/components/ProductList.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="item in props.productList" :key="item.id">
+                {{ item.name }} - {{ item.price }}원
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+
+    const props = defineProps({
+        productList: Object,
+    });
+
+    console.log(props.productList[0]);
+
+</script>
+
+<style scoped>
+
+</style>
