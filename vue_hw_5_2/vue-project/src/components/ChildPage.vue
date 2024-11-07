01f1f2cb3d14ab4c125627fa286e5842ef942bb3
김유정 <aabbc0908@naver.com>
2024-11-07 16:19:29 +0900

    commit

---
+++ vue_hw_5_2/vue-project/src/components/ChildPage.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div v-for="child in props.children" :key="child.name">
+            <h4>자식 페이지입니다.</h4>
+            이름 : {{ child.name }} <br>
+            나이 : {{ child.age }} <br>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    const props = defineProps({
+        children: Array,
+    });
+
+</script>
+
+<style scoped>
+
+</style>
