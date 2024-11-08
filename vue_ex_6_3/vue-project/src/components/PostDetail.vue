a62e91d8ed15452e9f5eeaade8de0f891ced6ca6
김유정 <aabbc0908@naver.com>
2024-11-08 17:20:15 +0900

    commit

--- vue_ex_6_3/vue-project/src/components/PostDetail.vue
+++ vue_ex_6_3/vue-project/src/components/PostDetail.vue
@@ -1,16 +1,15 @@
 <template>
     <div>
-        <p>번호 : {{ postId }}</p>
-        <!-- <p>제목 : {{ post.title }}</p>
-        <p>내용 : {{ post.content }}</p> -->
+        <p>번호 : {{ post.id }}</p>
+        <p>제목 : {{ post.title }}</p>
+        <p>내용 : {{ post.content }}</p>
     </div>
 </template>
 
 <script setup>
+
     import { ref, watch } from 'vue';
     import { useRoute } from 'vue-router';
-
-    const route = useRoute();
 
     const posts = ref([
         {id: 1, title: 'Post 1', content: 'Content 1'},
@@ -18,14 +17,14 @@
         {id: 3, title: 'Post 3', content: 'Content 3'},
     ]);
 
+    const route = useRoute();
     const postId = ref(route.params.id);
+    const post = ref({});
 
     watch(()=>route.params.id, (newId)=>{
         postId.value = newId;
-    });
-
-    const post = ref(() => {
-        return posts.value.find(item => item.id === postId.value);       
+        post.value = posts.value[postId.value-1];
+        console.log(post.value);
     });
 
 </script>
