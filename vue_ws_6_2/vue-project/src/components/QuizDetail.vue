87642e4bc6990217768387e54042cede2745ffd2
김유정 <aabbc0908@naver.com>
2024-11-08 16:57:02 +0900

    commit

--- vue_ws_6_2/vue-project/src/components/QuizDetail.vue
+++ vue_ws_6_2/vue-project/src/components/QuizDetail.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
         <h5>{{ quiz.pk }}번 문제. {{ quiz.question }}</h5>
-        <label for="answer">정답 입력</label>
-        <input type="text" id="answer">
+        <label :for="'answer + {{ quiz.pk }}'">정답 입력</label>
+        <input type="text" :id="'answer + {{ quiz.pk }}'">
     </div>
 </template>
 
