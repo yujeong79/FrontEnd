ae31d90f9abe449372c035963e81814be1bab439
김유정 <aabbc0908@naver.com>
2024-11-08 12:20:40 +0900

    commit

---
+++ vue_ex_6_2/vue-project/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <router-link :to="{name: 'home'}">Home</router-link> | 
+        <router-link :to="{name: 'about'}">About</router-link> | 
+        <router-link :to="{name: 'user', params: {id: username }}">User</router-link> | 
+      </nav>
+    </header>
+    
+    <router-view></router-view>
+  </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue';
+  import { RouterLink, RouterView } from 'vue-router';
+
+  const username = ref('admin');
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue_ex_6_2/vue-project/src/views/UserView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>User Profile</h1>
+        <p>Username: {{ userId }}</p>
+    </div>
+</template>
+
+<script setup>
+    import { ref } from 'vue';
+    import { useRoute } from 'vue-router';
+
+    const route = useRoute();
+    const userId = ref(route.params.id);
+</script>
+
+<style scoped>
+
+</style>
