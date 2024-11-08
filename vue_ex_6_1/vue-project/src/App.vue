dfd2502b5191c3c30b791b632de219fcef9ea888
김유정 <aabbc0908@naver.com>
2024-11-08 11:44:33 +0900

    commit

---
+++ vue_ex_6_1/vue-project/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <nav>
+      <router-link :to="{name: 'home'}">Home</router-link> | 
+      <router-link :to="{name: 'about'}">About</router-link>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script setup>
+  import { RouterLink, RouterView } from 'vue-router';
+</script>
+
+<style scoped>
+
+</style>
